b090bddbb11eba4bc5fc85c9a59975f66b9b1f70
Soumyadeep Debanth <soumyadeepp5467@gmail.com>
2026-07-04 19:31:40 +0530

    Time: 3 ms (25.75%), Space: 9.8 MB (7.07%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.c
@@ -0,0 +1,30 @@
+char* reverseVowels(char* s) {
+char *temp = malloc(strlen(s) + 1);
+int j = 0,count=0;
+
+for (int i = 0; s[i] != '\0'; i++) {
+    if (s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||
+        s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U') {
+        temp[j++] = s[i];
+    }
+}
+temp[j] = '\0';
+ for (int i=0; i<j/2; i++) {
+       char ch = temp[i];
+       temp[i] = temp[j-1-i];
+       temp[j-1-i] = ch;
+   }
+for (int i = 0; s[i] != '\0'; i++) {
+    if (s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||
+        s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U') {
+            count++;
+        s[i]=temp[count-1];
+    }
+}
+return s;
+free(temp);
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
