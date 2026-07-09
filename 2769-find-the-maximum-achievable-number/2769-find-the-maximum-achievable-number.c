e58ddfa73db2988376b8d396afa34294d5633c0d
Soumyadeep Debanth <soumyadeepp5467@gmail.com>
2026-07-09 22:03:33 +0530

    Time: 0 ms (100%), Space: 9.9 MB (60.13%) - LeetHub - Time: O(t), Space: O(1)

---
+++ 2769-find-the-maximum-achievable-number/2769-find-the-maximum-achievable-number.c
@@ -0,0 +1,10 @@
+int theMaximumAchievableX(int num, int t) {
+    for (int i=0;i<t;i++){
+        num++;
+    }
+    return (num+t);
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
