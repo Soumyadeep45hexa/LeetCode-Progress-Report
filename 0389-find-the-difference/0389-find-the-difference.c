a7b7a8cff0505bb036bfce87e97218f6a771fbb5
Soumyadeep Debanth <soumyadeepp5467@gmail.com>
2026-07-10 00:45:26 +0530

    Time: 0 ms (100%), Space: 8.8 MB (30.71%) - LeetHub - Time: O(, Space: O(?)

--- 0389-find-the-difference/0389-find-the-difference.c
+++ 0389-find-the-difference/0389-find-the-difference.c
@@ -2,23 +2,15 @@ char findTheDifference(char* s, char* t) {
     if (strlen(s)==0){
        return t[0];
     }
-    int flg;
+    char ans=0;
     int i;
     for (i=0;t[i]!='\0';i++){
-        flg=0;
-        for(int j=0;s[j]!='\0';j++){
-            if(t[i]==s[j]){
-                flg=1;
-                s[j]='#';
-                break;
-            }else{
-                flg=0;
-            }
-        } 
-        if  (flg==0)
-           break;
-    }
-    return t[i];
+        ans^=t[i];}
+    for(int j=0;s[j]!='\0';j++){
+        ans^=s[j];}
+         
+     
+    return ans;
 }
 
 // Synced seamlessly with LeetHub Pro
