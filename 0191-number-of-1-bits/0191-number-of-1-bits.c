abd6fc7c261a0a2b6ebc15c83bdd51e16d696b6b
Soumyadeep Debanth <soumyadeepp5467@gmail.com>
2026-07-11 23:35:22 +0530

    Time: 0 ms (100%), Space: 8.7 MB (97.02%) - LeetHub - Time: O, Space: O(?)

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.c
@@ -0,0 +1,10 @@
+int hammingWeight(int n) {
+    int count = 0;
+     while(n) { n = n & (n-1); count++; }
+    return count;
+  
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
