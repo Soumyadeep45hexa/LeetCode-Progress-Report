18cc88dbef69a186278d695bde426a965b5290e9
Soumyadeep Debanth <soumyadeepp5467@gmail.com>
2026-07-12 20:48:15 +0530

    Time: 3 ms (76.83%), Space: 104.8 MB (16.77%) - LeetHub - Time: O(?), Space: O(?)

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.c
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.c
@@ -1,21 +1,22 @@
 /** * Note: The returned array must be malloced, assume caller calls free().
  */
 int* rearrangeArray(int* nums, int numsSize, int* returnSize) {
-    *returnSize=numsSize;
-    int *ans=(int *)malloc((*returnSize)*sizeof(int));
+
+    int *ans=(int *)malloc(numsSize*sizeof(int));
     int pos=0;
     int neg=1;
     for (int i=0;i<numsSize;i++){
         if (nums[i]>0){
-            // fix this
+        
             ans[pos] = nums[i]; 
             pos=pos+2;
         }else if (nums[i]<0){
-            // fix this
+           
             ans[neg] = nums[i]; 
             neg=neg+2;
         }
     }
+    *returnSize=numsSize;
     return ans;
 }
 
