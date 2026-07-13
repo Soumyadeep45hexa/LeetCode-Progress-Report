8b610828774cce05b8ca279ea358d8dca643e520
Soumyadeep Debanth <soumyadeepp5467@gmail.com>
2026-07-13 20:55:24 +0530

    Time: 50 ms (50.83%), Space: 19.3 MB (20.72%) - LeetHub

---
+++ 0218-contains-duplicate/0218-contains-duplicate.c
@@ -0,0 +1,16 @@
+int compare(const void *a,const void *b){
+    return *(int *)a-*(int *)b;
+    }
+bool containsDuplicate(int* nums, int numsSize) { 
+    qsort(nums,numsSize,sizeof(int),compare);
+for (int i=0;i<numsSize-1;i++){
+    if (nums[i]==nums[i+1]){
+        return true;
+    }
+  }
+    return false;
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
