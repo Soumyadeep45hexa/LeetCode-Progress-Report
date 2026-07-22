c47e9857224048c5b2811f48176e2d0793adc2bc
Soumyadeep Debanth <soumyadeepp5467@gmail.com>
2026-07-22 12:03:24 +0530

    Time: 0 ms (100%), Space: 8.9 MB (5.59%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.c
@@ -0,0 +1,32 @@
+int bitwiseComplement(int n) {
+   int *ans=(int *)malloc(32*sizeof(int));
+   int k=0;
+   if (n==0)
+   return 1;
+   while(n){
+    int d=n&1;
+    ans[k++]=d^1;
+    n>>=1;
+   }
+   int left=0;
+   int right=k-1;
+   while(left<right){
+    int temp=ans[left];
+    ans[left]=ans[right];
+    ans[right]=temp;
+    left++;
+    right--;
+     }
+     int sum=0;
+     int len=k-1;
+     for (int i=0;i<=k-1;i++){
+        
+        sum+=ans[i]*pow(2,len);
+        len--;
+     }
+     return sum;
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
