4f28be19ceb547b529d47fe9a3e593ef5530bc23
Soumyadeep Debanth <soumyadeepp5467@gmail.com>
2026-07-29 09:37:04 +0530

    Time: 0 ms (100%), Space: 11.1 MB (90.98%) - LeetHub - Time: O(n*m), Space: O(n)

---
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.c
@@ -0,0 +1,21 @@
+char* mapWordWeights(char** words, int wordsSize, int* weights, int weightsSize) {
+    int returnSize=wordsSize;
+    char *str =(char *)malloc((returnSize+1)*sizeof(char));
+    int k=0;
+    for (int i=0;i<wordsSize;i++){
+        int len=strlen(words[i]);
+        int sum=0;
+        for (int j=0;j<len;j++){
+             sum+=weights[words[i][j]-'a'];
+        }
+        int mod=sum%26;
+        str[k++]='z'- mod;
+    }
+    str[k]='\0';
+    returnSize=k;
+    return str;
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
