c1977b04a75f0ae539cd2328eb005f8789dab4a6
Soumyadeep Debanth <soumyadeepp5467@gmail.com>
2026-08-08 22:36:58 +0530

    Time: 0 ms (100%), Space: 8.3 MB (90.01%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 0292-nim-game/0292-nim-game.c
@@ -0,0 +1,8 @@
+bool canWinNim(int n) {
+ if(n%4==0) return false;
+ return true;
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
